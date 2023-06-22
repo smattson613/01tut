d226de34d42cede948c45dad59f9ca6dbccd0d15
Stephen Mattson <smattson613@gmail.com>
2023-06-22 18:06:44 -0400

    Controlled Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
@@ -6,8 +7,8 @@ import { useState } from 'react';
 
 function App() {
   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
-
-  const [newItem, setNewItem] = useState('')
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems)
@@ -45,9 +46,13 @@ function App() {
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
+      /> 
+      <SearchItem
+        search={search}
+        setSearch={setSearch} 
       />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
